a38df66c740a5464957d314408e7190739284a85
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-04-20 12:49:49 +0100

    Complete exercise 1

--- AbilityScoreCalculator/Program.cs
+++ AbilityScoreCalculator/Program.cs
@@ -28,9 +28,63 @@ namespace AbilityScoreCalculator
                 Score = added;
             }
         }
+
+       
+        /// <summary>
+        /// Writes a prompt and reads an int value from the console.
+        /// </summary>
+        /// <param name="lastUsedValue">The default value.</param>
+        /// <param name="prompt">Prompt to the print to the console.</param>
+        /// <returns>The int value read, or the default value if uable to parse</returns>
+
+        private static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write($"{prompt} [{lastUsedValue}]: ");
+            string newValueString = Console.ReadLine();
+             if (int.TryParse(newValueString, out int newValueInt))
+            {
+                Console.WriteLine($"\tusing value {newValueInt}");
+                return newValueInt;
+            } else
+            {
+                Console.WriteLine($"\tusing default value {lastUsedValue}");
+                return lastUsedValue;
+            }
+        }
+
+        private static double ReadDouble(double lastUsedValue, string prompt)
+        {
+
+            Console.Write($"{prompt} [{lastUsedValue}]: ");
+            string newValueString = Console.ReadLine();
+            if (double.TryParse(newValueString, out double newValueDouble))
+            {
+                Console.WriteLine($"\tusing value {newValueDouble}");
+                return newValueDouble;
+            }
+            else
+            {
+                Console.WriteLine($"\tusing default value {lastUsedValue}");
+                return lastUsedValue;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine($"Calculated ability score: {calculator.Score}");
+                Console.WriteLine("Press Q to quit, any other key to continue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q') || (keyChar == 'q')) return;
+            }
         }
     }
 }
